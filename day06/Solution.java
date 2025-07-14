339e6f797ea6a3a3740a28147eed54f7260f6b75
ImSH123 <park040555@gmail.com>
2025-07-14 23:24:20 +0900

    day06 complete

---
+++ day06/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+
+        for(int i=0; i<prices.length; i++){
+            int seconds = 0;
+            for(int j=i+1; j<prices.length; j++){
+                seconds++;
+                if(prices[i]>prices[j]){
+                    break;
+                }
+            }
+            answer[i] = seconds;
+        }
+        return answer;
+    }
+}
